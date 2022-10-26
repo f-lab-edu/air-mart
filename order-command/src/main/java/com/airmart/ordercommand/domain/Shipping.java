e5870b1d51bde23474e184c07bd544bbf4f924e8
Hero <icraft2170@gmail.com>
2022-10-26 17:25:45 +0900

    feature : #25 Shipping Domain 생성자및 컬럼 설명 추가

--- order-command/src/main/java/com/airmart/ordercommand/domain/Shipping.java
+++ order-command/src/main/java/com/airmart/ordercommand/domain/Shipping.java
@@ -3,13 +3,19 @@ package com.airmart.ordercommand.domain;
 import com.airmart.ordercommand.domain.embed.Address;
 
 import javax.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Entity
+@Getter @NoArgsConstructor @AllArgsConstructor
 public class Shipping extends BaseAuditor{
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @Column(nullable = false, columnDefinition = "송장 번호")
     private String shippingCode;
     @Embedded
     private Address address;
+    @Column(columnDefinition = "배송 시 요청사항")
     private String shippingRequests;
 }
